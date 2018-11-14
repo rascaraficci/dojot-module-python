872b61f000cb082ee81afc92971e3174b8f4ea65
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2018-11-14 10:15:47 -0200

    improving setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,39 @@
 # -*- coding: utf-8 -*-
-
+import io
 from setuptools import setup, find_packages
+from collections import OrderedDict
+
+with io.open('README.md', 'rt', encoding='utf8') as f:
+    readme = f.read()
 
 setup(
-      name='dojot.module',
-      version='0.53',
-      description='Library for new dojot modules development',
-      url='http://github.com/dojot/dojot-module-python',
-      author='Matheus Campanha Ferreira',
-      author_email='campanha@cpqd.com.br',
-      license='GPL-3.0',
-      packages=find_packages(exclude=['test']),
-      install_requires=['requests','kafka-python', 'colorlog'],
-      zip_safe=False
+    name='dojot.module',
+    version='0.0.1-alpha.1',
+    url='http://github.com/dojot/dojot-module-python',
+    project_urls=OrderedDict((
+        ('Code', 'https://github.com/dojot/dojot-module-python.git'),
+        ('Issue tracker', 'https://github.com/dojot/dojot-module-python/issues'),
+    )),
+    license='GPL-3.0',
+    author='Matheus Campanha Ferreira',
+    author_email='campanha@cpqd.com.br',
+    maintainer='dojot team',
+    description='Library for new dojot modules development',
+    long_description=readme,
+    packages=["dojot.module", "dojot.module.kafka"],
+    include_package_data=True,
+    zip_safe=False,
+    platforms=[any],
+    install_requires=[
+        'requests==2.18.0',
+        'kafka-python==1.4.3',
+        'colorlog==3.1.4',
+        'pyaml==17.12.1'
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=3",
+            "mock==2.0.0"
+        ]
+    }
 )
