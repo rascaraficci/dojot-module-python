14f2d94eccce8c1a657f0be33f666d68bf941553
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2018-11-15 20:15:43 -0200

    adding proper configuration to config.py

--- dojot/module/config.py
+++ dojot/module/config.py
@@ -11,23 +11,115 @@ class Config:
     This class contains all needed configuration for this library
     """
     def __init__(self, config=None):
+        """
+        Config constructor
+
+        :type config: Config or None
+        :param config: A configuration dictionary. If set, all its attributes
+        will be set to this object.
+
+        Any top level key will overwrite the default configuration, i.e., setting
+        a `kafka` object to config param will overwrite all Kafka configuration.
+        An example of such dictionary is:
+
+        .. code-block:python
+
+            {
+                "kafka" = {
+                    "producer": {
+                        "client.id": "kafka",
+                        "metadata.broker.list": "kafka:9092",
+                        "compression.codec": "gzip",
+                        "retry.backoff.ms": 200,
+                        "message.send.max.retries": 10,
+                        "socket.keepalive.enable": True,
+                        "queue.buffering.max.messages": 100000,
+                        "queue.buffering.max.ms": 1000,
+                        "batch.num.messages": 1000000,
+                        "dr_cb": True
+                    },
+                    "consumer": {
+                        "group.id": "my-module",
+                        "metadata.broker.list": "kafka:9092"
+                    }
+                }
+                "data_broker" = {
+                    "url": "http://data-broker"
+                }
+
+                "auth" = {
+                    "url": "http://auth:5000"
+                }
+
+                "dojot" = {
+                    "management_service": "dojot-management",
+                    "subjects": {
+                        "tenancy": "dojot.tenancy",
+                        "devices": "dojot.device-manager.device",
+                        "device_data": "device-data,
+                    }
+                }
+            }
+
+        .. warning: If set, the `dojot` section should be in sync with all
+        other modules. Otherwise this module won't work.
+
+        .. note: The Kafka object is straight from librdkafka configuration,
+        separated into producer and consumer subobjects. For more information
+        about this configuration, you should check its documentation.
+        """
+        self.load_defaults()
+        self.load_env()
         if config is not None:
-            self.kafka = config["kafka"]
-            self.data_broker = config["data_broker"]
-            self.auth = config["auth"]
-            self.dojot = config["dojot"]
-        else:
-            self.load_defaults()
+            if "kafka" in config: self.kafka = config["kafka"]
+            if "data_broker" in config: self.data_broker = config["data_broker"]
+            if "auth" in config: self.auth = config["auth"]
+            if "dojot" in config: self.dojot = config["dojot"]
 
 
     def load_defaults(self):
         """
-        Load default configuration
+        Load default configuration, which is:
+
+        .. code-block:yaml
+
+            kafka:
+                producer:
+                    client.id: "kafka"
+                    metadata.broker.list: "kafka:9092"
+                    compression.codec: "gzip"
+                    retry.backoff.ms: 200
+                    message.send.max.retries: 10
+                    socket.keepalive.enable: True
+                    queue.buffering.max.messages: 100000
+                    queue.buffering.max.ms: 1000
+                    batch.num.messages: 1000000
+                    dr_cb: true
+                consumer:
+                    group.id: "my-module"
+                    metadata.broker.list: "kafka:9092"
+            data_broker:
+                url: "http://data-broker"
+            auth:
+                url: "http://auth:5000"
+            dojot:
+                management_service: "dojot-management"
+                subjects:
+                    tenancy: "dojot.tenancy"
+                    devices: "dojot.device-manager.device"
+                    device_data: "device-data"
+
+        .. warning: calling this function will overwrite any previously set
+        configuration. Also setting any configuration *after* Kafka is started
+        or any Messenger object is created will have no effect on them.
+
+        .. warning: If set, the `dojot` section should be in sync with all
+        other modules. Otherwise this module won't work.
         """
         self.kafka = {
             "producer": {
                 "client.id": "kafka",
-                "metadata.broker.list": os.environ.get('KAFKA_HOSTS', "kafka:9092"),
+                "metadata.broker.list": "kafka:9092",
                 "compression.codec": "gzip",
                 "retry.backoff.ms": 200,
                 "message.send.max.retries": 10,
@@ -38,45 +130,108 @@ class Config:
                 "dr_cb": True
             },
             "consumer": {
-                "group.id": os.environ.get('KAFKA_GROUP_ID', "data-broker"),
-                "metadata.broker.list": os.environ.get('KAFKA_HOSTS', "kafka:9092")
+                "group.id": "my-module",
+                "metadata.broker.list": "kafka:9092"
             }
         }
         self.data_broker = {
-            "url": os.environ.get('DATA_BROKER_URL', "http://data-broker")
+            "url": "http://data-broker"
         }
 
         self.auth = {
-            "url": os.environ.get('AUTH_URL', "http://auth:5000")
+            "url": "http://auth:5000"
         }
 
         self.dojot = {
-            "management_service": os.environ.get('DOJOT_SERVICE_MANAGEMENT', "dojot-management"),
+            "management_service": "dojot-management",
             "subjects": {
-                "tenancy": os.environ.get('DOJOT_SUBJECT_TENANCY', "dojot.tenancy"),
-                "devices": os.environ.get('DOJOT_SUBJECT_DEVICES', "dojot.device-manager.device"),
-                "device_data": os.environ.get('DOJOT_SUBJECT_DEVICE_DATA', "device-data")
+                "tenancy": "dojot.tenancy",
+                "devices": "dojot.device-manager.device",
+                "device_data": "device-data"
             }
         }
 
-        self.load_env()
 
     def load_file(self, config_file):
         """
-        Load configuration from a file
+        Load configuration from a file. 
+        Any top level key will overwrite the default configuration, i.e., having
+        a `kafka` item in the file will overwrite all Kafka configuration.
+        Such file should have the following sections:
+
+        .. code-block:yaml
+
+            kafka:
+                producer:
+                    client.id: "kafka"
+                    metadata.broker.list: "kafka:9092"
+                consumer:
+                    group.id: "data-broker"
+                    metadata.broker.list: "kafka:9092"
+                data_broker:
+                    url: "http://data-broker"
+                auth:
+                    url: "http://auth:5000"
+                dojot:
+                    management_service: "dojot-management"
+                    subjects:
+                        tenancy: "dojot.tenancy"
+                        devices: "dojot.device-manager.device"
+                        device_data: "device-data"
+
+        .. warning: calling this function will overwrite any previously set
+        configuration. Also setting any configuration *after* Kafka is started
+        or any Messenger object is created will have no effect on them.
+
+        .. warning: If set, the `dojot` section should be in sync with all
+        other modules. Otherwise this module won't work.
+
+        .. note: The Kafka object is straight from librdkafka configuration,
+        separated into producer and consumer subobjects. For more information
+        about this configuration, you should check its documentation.
         """
+        self.load_defaults()
         parsed_config = yaml.safe_load(open(config_file))
-        self.kafka = parsed_config["kafka"]
-        self.data_broker = parsed_config["data_broker"]
-        self.auth = parsed_config["auth"]
-        self.dojot = parsed_config["dojot"]
+        print(f"Parsed config: {parsed_config}")
+
+        if "kafka" in parsed_config: print("kafka")
+        if "data_broker" in parsed_config: print("data_broker")
+        if "auth" in parsed_config: print("auth")
+        if "dojot" in parsed_config: print("dojot")
+
+        if "kafka" in parsed_config: self.kafka = parsed_config["kafka"]
+        if "data_broker" in parsed_config: self.data_broker = parsed_config["data_broker"]
+        if "auth" in parsed_config: self.auth = parsed_config["auth"]
+        if "dojot" in parsed_config: self.dojot = parsed_config["dojot"]
 
         self.load_env()
 
     def load_env(self):
         """
-        Load configuration from environment variables
+        Load configuration from environment variables.
+
+        Any environment variable will overwrite the default configuration.
+        Check load_defaults() function.
+
+        The list of envirnoment variables are:
+
+        - KAFKA_HOSTS: a comma-separated list of hosts where an instance of Kafka
+          is running. This will affect the `metadata.broker.list` parameter for
+          both Kafka consumer and producer.
+        - KAFKA_GROUP_ID: The Kafka consumer group ID to be used.
+        - DATA_BROKER_URL: Where DataBroker service can be reached.
+        - AUTH_URL: Where Auth service can be reached.
+        - DOJOT_SERVICE_MANAGEMENT: service to be used when asking DataBroker
+          for management topics (such as tenancy-related topics)
+        - DOJOT_SUBJECT_TENANCY: Subject to be used when asking DataBroker for
+          tenancy topics.
+        - DOJOT_SUBJECT_DEVICES: Subject to be used when asking DataBroker for
+          device topics.
+        - DOJOT_SUBJECT_DEVICE_DATA: Subject to be used when asking DataBroker
+          for device data topics.
+
         """
+
         self.kafka["producer"]["metadata.broker.list"] = os.environ.get(
             'KAFKA_HOSTS', self.kafka["producer"]["metadata.broker.list"])
 
