e7e9bf06c53d88998a8e8a660c9b49e5e35bd5c4
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2018-11-26 22:38:01 -0200

    feat: adding tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
 import unittest
-from unittest.mock import patch, MagicMock, Mock
+from unittest.mock import patch, MagicMock, Mock, DEFAULT
 from dojot.module import Auth, Config
 
 def test_get_management_token_ok():
@@ -17,10 +17,12 @@ def test_get_management_token_ok():
     token = Auth.get_management_token(mockSelf)
     assert token is not None
 
-def test_get_tenants_ok():
+def test_get_tenants():
     config = Mock(
         auth={
-            "url": "http://sample-url"
+            "url": "http://sample-url",
+            "timeout_sleep": 1,
+            "connection_retries": 3
         },
         dojot={
             "management": {
@@ -33,6 +35,11 @@ def test_get_tenants_ok():
         get_management_token=Mock(return_value="123"),
         config=config
     )
+
+    def reset_scenario():
+        mockSelf.get_management_token.reset_mock()
+
+
     patchResponse = patch("requests.Response.json", return_value = {"tenants": "123"})
     patchReqGet = patch("requests.get", return_value=requests.Response)
     with patchResponse, patchReqGet as mockReqGet:
@@ -42,3 +49,36 @@ def test_get_tenants_ok():
         })
         assert tenants == "123"
     
+    reset_scenario()
+    patchResponse = patch("requests.Response.json", return_value = {"tenants": "123-connection"})
+    patchReqGet = patch("requests.get", return_value=requests.Response, side_effect=[requests.exceptions.ConnectionError, DEFAULT])
+    mockSelf.get_management_token.return_value = "token-123-connection"
+    with patchResponse, patchReqGet as mockReqGet:
+        tenants = Auth.get_tenants(mockSelf)
+        mockReqGet.assert_called_with("http://sample-url/admin/tenants", headers = {
+            "authorization": "Bearer token-123-connection"
+        })
+        assert tenants == "123-connection"
+    
+    reset_scenario()
+    patchResponse = patch("requests.Response.json", return_value = {"tenants": "123-timeout"})
+    patchReqGet = patch("requests.get", return_value=requests.Response, side_effect=[requests.exceptions.Timeout, DEFAULT])
+    mockSelf.get_management_token.return_value = "token-123-timeout"
+    with patchResponse, patchReqGet as mockReqGet:
+        tenants = Auth.get_tenants(mockSelf)
+        mockReqGet.assert_called_with("http://sample-url/admin/tenants", headers = {
+            "authorization": "Bearer token-123-timeout"
+        })
+        assert tenants == "123-timeout"
+    
+
+    reset_scenario()
+    patchResponse = patch("requests.Response.json", return_value = {"tenants": "123-redirects"})
+    patchReqGet = patch("requests.get", return_value=requests.Response, side_effect=[requests.exceptions.TooManyRedirects, DEFAULT])
+    mockSelf.get_management_token.return_value = "token-123-redirects"
+    with patchResponse, patchReqGet as mockReqGet:
+        tenants = Auth.get_tenants(mockSelf)
+        mockReqGet.assert_called_with("http://sample-url/admin/tenants", headers = {
+            "authorization": "Bearer token-123-redirects"
+        })
+        assert tenants is None
